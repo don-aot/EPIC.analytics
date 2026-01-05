e04069623014c930cbe6e1269307d3cd8ad1ad93
don-aot <don.varghese@aot-technologies.com>
2026-01-05 09:35:38 -0800

    CENTR-51:Display Last Accessed

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
-  dts: true,
+  dts: {
+    resolve: false, // Skip resolving types from external dependencies
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
@@ -11,4 +13,5 @@ export default defineConfig({
   bundle: true,
   outDir: 'dist',
 });
+
 
